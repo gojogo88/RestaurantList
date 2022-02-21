adbf0086daa8a83475f35696c9cb81574dd7c372
Jonathan Go <gojonathan.dev@gmail.com>
2022-02-21 19:01:05 +0100

    Initial Commit

---
+++ RestaurantList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RestaurantList
+//
+//  Created by Jonathan Go on 21.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
